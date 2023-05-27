fd124560fa730c893c1ba07f1c90370332068b12
ZoleeBossTheBossPericulos <zfelhazi@yahoo.com>
2023-05-27 20:58:08 +0300

    feat: index UI done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <Layout title={"Home"}>
-      <h1>Home</h1>
+      <div className="flex flex-col items-center justify-center h-[80vh]">
+        <h1 className="text-4xl font-bold mb-4">
+          Welcome to the AuctiOn Website!
+        </h1>
+        <p className="text-lg text-gray-600 mb-8">
+          Explore a wide range of auctions and bid on your favorite items.
+        </p>
+        <div>
+          <button className="px-6 py-3 bg-amber-500 hover:bg-amber-600 mr-4 text-white rounded-md shadow-lg">
+            Get Started
+          </button>
+          <button className="px-6 py-3 bg-slate-500 hover:bg-slate-600 text-white rounded-md shadow-lg">
+            Log in
+          </button>
+        </div>
+      </div>
     </Layout>
   );
 }
